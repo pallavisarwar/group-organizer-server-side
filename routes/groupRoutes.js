da4227348dff8e5532c8ac2a51d94a126ad6dca9
Aviv <aviv.dror@gmail.com>
2024-07-24 23:23:43 +0200

    groups api update

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -50,7 +50,7 @@ router.get("/:id", (req, res, next) => {
 
 router.post("/", (req, res, next) => {
   if (!req.body.title || req.body.title === "") {
-    res.status(400).json({ error: "Group Title must be provided" });
+    res.status(400).json({ error: "New group title must be provided" });
     return;
   }
   const data = {
@@ -79,6 +79,37 @@ router.post("/", (req, res, next) => {
     console.error("Error while creating group", e.message);
     next(e);
   }
+});
+
+router.put("/:id", (req, res, next) => {
+  if (!req.params.id || req.params.id <= 0) {
+    res.status(404).json({ error: "Group not found" });
+    return;
+  }
+  const data = {
+    GroupId: req.params.id,
+    Title: req.body.title,
+    Description: req.body.description,
+    ImageURL: req.body.imageUrl,
+  };
+  const sql = `UPDATE Groups SET
+  Title = COALESCE("${data.Title}", Title),
+  Description = COALESCE("${data.Description}", Description),
+  ImageURL = COALESCE("${data.ImageURL}", ImageURL)
+  WHERE GroupId = ${req.params.id}`;
+  try {
+    db.run(sql, function (err, result) {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: "success",
+        data,
+        changes: this.changes,
+      });
+    });
+  } catch (e) {}
 });
 
 module.exports = router;
